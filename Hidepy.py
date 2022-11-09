3818222f1c3c05abd27c0f2ba6cccb3a92829c40
Pol8y <64278895+Pol8y@users.noreply.github.com>
2022-11-09 17:51:50 +0100

    Update Hidepy.py

--- Hidepy.py
+++ Hidepy.py
@@ -10,7 +10,7 @@ def toBinary(a):
   for i in a:
     l.append(ord(i))
   for i in l:
-    m.append(int(bin(i)[2:]))
+    m.append(bin(i))
   return m
 
 
@@ -24,12 +24,22 @@ img = im1.load()
 print(xs,ys)
 for x in range(0,xs):
 	for y in range (0,ys):
-		[r,g,b]=im1.getpixel((x,y))
-		r = r -250
-		g = g -250
-		b = b -250
-		R = bin(255)[2:]
-		value = (r,g,b)
-		im1.putpixel((x,y), value)
-print(r,g,b,R)
+		for k in i:
+			[r,g,b]=im1.getpixel((x,y))
+			r = r - int(k, 2)
+			print(r)
+			value = (r,g,b)
+			im1.putpixel((x,y), value)
+print(r,g,b)
 im1.save('new.jpg')
+im2 = Image.open('new.jpg')
+img = im2.load()
+[xxs,yys] = im2.size
+for x in range(0,xs):
+	for y in range (0,ys):
+		for xx in range(0,xxs):
+			for yy in range (0,yys):
+				[r,g,b]=im1.getpixel((x,y))
+				[r2,g2,b2]=im2.getpixel((xx,yy))
+				k= r -r2
+				print(bin(k))
