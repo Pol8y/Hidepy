a615a45f5f17fe962803a1087e856d4be9a4e827
Pol8y <64278895+Pol8y@users.noreply.github.com>
2022-12-23 02:50:34 +0100

    Update Hidepy.py

--- Hidepy.py
+++ Hidepy.py
@@ -1,45 +1,69 @@
 #!/usr/bin/python3
 
-#requirements
-from PIL import Image
-import math
+import cv2
+import sys
+
+def encode_text_in_image(image, text):
+    # Convert the image to a 3-channel BGR image
+    image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    
+    # Convert the text to a list of ASCII values
+    text_ascii = [ord(c) for c in text]
+    
+    # Encode the text in the least significant bits of the image pixels
+    for i, ascii_value in enumerate(text_ascii):
+        # Get the i-th pixel value
+        pixel = image[i // image.shape[1], i % image.shape[1]]
+        
+        # Set the least significant bit of each channel to the corresponding
+        # bit of the ASCII value
+        pixel[0] = (pixel[0] & 0xFE) | (ascii_value & 0x01)
+        pixel[1] = (pixel[1] & 0xFE) | ((ascii_value & 0x02) >> 1)
+        pixel[2] = (pixel[2] & 0xFE) | ((ascii_value & 0x04) >> 2)
+        
+        # Update the i-th pixel value
+        image[i // image.shape[1], i % image.shape[1]] = pixel
+        
+    return image
+
+def decode_text_from_image(image):
+    # Convert the image to a 1-channel grayscale image
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Initialize an empty list to store the decoded ASCII values
+    text_ascii = []
+    
+    # Decode the text from the least significant bits of the image pixels
+    for i in range(image.shape[0] * image.shape[1]):
+        # Get the i-th pixel value
+        pixel = image[i // image.shape[1], i % image.shape[1]]
+        
+        # Extract the least significant bits of each channel
+        ascii_value = (pixel[0] & 0x01) | ((pixel[1] & 0x01) << 1) | ((pixel[2] & 0x01) << 2)
+        
+        # If the ASCII value is 0, we have reached the end of the text
+        if ascii_value == 0:
+            break
+            
+        # Add the ASCII value to the list
+        text_ascii.append(ascii_value)
+    
+    # Convert the list of ASCII values to a string
+    text = ''.join(chr(c) for c in text_ascii)
+    
+    return text
+
+# Read the image
+image = cv2.imread('image.jpg', cv2.IMREAD_GRAYSCALE)
 
 
-def toBinary(a):
-  l,m=[],[]
-  for i in a:
-    l.append(ord(i))
-  for i in l:
-    m.append(bin(i))
-  return m
+# Check if the -d flag is present to decode the text
+if '-d' in sys.argv:
+    text = decode_text_from_image(image)
+    print(text)
 
-
-inp = input('Write the sentence you want to hide:\n')
-i=toBinary(inp)
-print(' '.join(map(str, i)))
-inp2 = input('where\'s the image you want to hide your secret into?\n')
-im1 = Image.open(inp2)
-img = im1.load()
-[xs,ys]=im1.size
-print(xs,ys)
-for x in range(0,xs):
-	for y in range (0,ys):
-		for k in i:
-			[r,g,b]=im1.getpixel((x,y))
-			r = r - int(k, 2)
-			print(r)
-			value = (r,g,b)
-			im1.putpixel((x,y), value)
-print(r,g,b)
-im1.save('new.jpg')
-im2 = Image.open('new.jpg')
-img = im2.load()
-[xxs,yys] = im2.size
-for x in range(0,xs):
-	for y in range (0,ys):
-		for xx in range(0,xxs):
-			for yy in range (0,yys):
-				[r,g,b]=im1.getpixel((x,y))
-				[r2,g2,b2]=im2.getpixel((xx,yy))
-				k= r -r2
-				print(bin(k))
+# Check if the -e flag is present to encode the text
+if '-e' in sys.argv:
+    text = input('Enter the text to be hidden: ')
+    image_with_hidden_text = encode_text_in_image(image, text)
+    cv2.imwrite('image_with_hidden_text.jpg', image_with_hidden_text)
